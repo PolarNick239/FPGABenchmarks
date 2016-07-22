51c3125f4d847a271268db03b482ccdd06313225
PolarNick239 <PolarNick239@gmail.com>
2016-07-23 00:26:00 +0300

    minors (-mfma removed, -msse -msse2 added, minor benchmark output changes)

--- benchmarks/mandelbrot/src/main_bench.cpp
+++ benchmarks/mandelbrot/src/main_bench.cpp
@@ -35,10 +35,10 @@ int main() {
 
     processors.push_back(pair<string, MandelbrotProcessor&>(string("CPU AVX2\t") + threads_suffix, cpu_avx));
     processors.push_back(pair<string, MandelbrotProcessor&>(string("CPU AVX2\t") + single_thread_suffix, cpu_avx_single));
-    processors.push_back(pair<string, MandelbrotProcessor&>(string("CPU SSE\t\t") + threads_suffix, cpu_sse));
-    processors.push_back(pair<string, MandelbrotProcessor&>(string("CPU SSE\t\t") + single_thread_suffix, cpu_sse_single));
-    processors.push_back(pair<string, MandelbrotProcessor&>(string("CPU\t\t\t") + threads_suffix, cpu));
-    processors.push_back(pair<string, MandelbrotProcessor&>(string("CPU\t\t\t") + single_thread_suffix, cpu_single));
+    processors.push_back(pair<string, MandelbrotProcessor&>(string("CPU SSE \t") + threads_suffix, cpu_sse));
+    processors.push_back(pair<string, MandelbrotProcessor&>(string("CPU SSE \t") + single_thread_suffix, cpu_sse_single));
+    processors.push_back(pair<string, MandelbrotProcessor&>(string("CPU     \t") + threads_suffix, cpu));
+    processors.push_back(pair<string, MandelbrotProcessor&>(string("CPU     \t") + single_thread_suffix, cpu_single));
 
     for (size_t i = 0; i < processors.size(); i++) {
         string label = processors[i].first;
